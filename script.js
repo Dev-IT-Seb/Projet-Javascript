d4e42e21ed4d40e62b4064b98b6c2e2f2627efca
sebastien c. <schatel.pro@pm.me>
2025-03-05 14:02:08 +0100

    test:exericeGames_05.03.2025_partie2

--- script.js
+++ script.js
@@ -12,15 +12,20 @@
 // }
 //
 // Exercice GAME
-let Harry = 29; 
+let Harry = 30; 
+let Voldemort = "Voldemort"; 
+
 // Conditions si 
 if (Harry >= 50){
     alert("Harry dit: J'attaque normalement.");
 }
+// EFFET POISON SUR POINT DE VIE PDT L'ATTAQUE
 else if (Harry >= 30 && Harry <= 50){
-
-    alert("Harry dit: J'attaque moins fort");
-}
-else {
-    alert("Harry dit: Je dois fuir le combat !");
-}
+    // Conversation après l'attaque
+    alert( "Harry dit: J'attaque moins fort.. Car j'ai contracté son poison..." );
+    alert( Voldemort + " " + "dit : HAAAA tu me fais mal mais tu as hérité de mon poison, héhéhé !");
+    alert( "Harry dit : Comme l'adversaire est contaminé, je perd 1 point de vie, snif :=(" )
+    // Perte de 1pt sur la vie du Hero
+    Harry--;
+    alert("voici mes points de vie actuel à cause du poison :" + " " + Harry)
+}   
