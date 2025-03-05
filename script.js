61248d5cf90e4d44d26f98ceb12a8cc65aab4280
sebastien c. <schatel.pro@pm.me>
2025-03-05 14:45:36 +0100

    test:exerice_For_While_BREAK

--- script.js
+++ script.js
@@ -11,7 +11,32 @@
 //    console.log("Après une réduction de" + " " + solde + " " +"%, le prix final est de" + " " + resultatLast + " euros")
 // }
 //
-// FOR
-for ( let PointsVie = 1; PointsVie <= 5; PointsVie++){
-   console.log("Mes points de vie après la potion de santé:" + PointsVie);
-}
+// ------------ FOR -----------
+//for ( let PointsVie = 1; PointsVie <= 5 ; PointsVie++){
+//
+//   console.log("Mes points de vie après la potion de santé:" + PointsVie);
+//}
+// ----------- WHILE ----------
+//let pointvies = 10;
+//
+//while ( pointvies > 0 ){
+//
+//    console.log("Il me reste" + " " + pointvies + " " + "points de vie. Continue à jouer Harry !")
+//    pointvies--;
+//}
+//console.log("Game Over !")
+//
+//
+// --------------- BREAK --------------
+//let compteur = 1;
+//
+//while(true){
+//    console.log("Degat au joueur" + " " + compteur +"pts de perdu")
+//
+//    if(compteur === 5){
+//
+//      console.log("L'attaque est terminée");
+//        break
+//    }
+//    compteur ++;
+//}
