729b9e24eec629cf0690830e8caf11eb891311ff
sebastien c. <schatel.pro@pm.me>
2025-03-05 12:33:55 +0100

    test:exerice3

---
+++ script.js
@@ -0,0 +1,12 @@
+// Exerice 3
+// let user = prompt("Saisie le prix de ton article");
+// let solde = prompt("Saisie le pourcentage de la remise de cet article");
+// let resultatFirst = ( user * solde / 100 )
+// let resultatLast = (user - resultatFirst) 
+// 
+// if (user <= 0){
+//    console.log("Veuillez saisir une valeur valide de votre article !")
+// }
+// else {
+//    console.log("Après une réduction de" + " " + solde + " " +"%, le prix final est de" + " " + resultatLast + " euros")
+// }
