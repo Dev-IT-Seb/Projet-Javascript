aa1a55bdeb962b45f3181b78f532c7277013fcab
sebastien c. <schatel.pro@pm.me>
2025-03-05 20:09:52 +0100

    test:Games

---
+++ exercices.js
@@ -0,0 +1,53 @@
+// ---------------------    GAMES 1er    ---------------------
+//let Harry = 30; 
+//let Voldemort = "Voldemort"; 
+//
+// Conditions si 
+//if (Harry >= 50){
+//    alert("Harry dit: J'attaque normalement.");
+//}
+// EFFET POISON SUR POINT DE VIE PDT L'ATTAQUE
+//else if (Harry >= 30 && Harry <= 50){
+    // Conversation après l'attaque
+//    alert( "Harry dit: J'attaque moins fort.. Car j'ai contracté son poison..." );
+ //   alert( Voldemort + " " + "dit : HAAAA tu me fais mal mais tu as hérité de mon poison, héhéhé !");
+//    alert( "Harry dit : Comme l'adversaire est contaminé, je perd 1 point de vie, snif :=(" )
+    // Perte de 1pt sur la vie du Hero
+//    Harry--;
+//    alert("voici mes points de vie actuel à cause du poison :" + " " + Harry)
+//}   
+// ---------------------    EXERCICE 3   ---------------------
+// Exerice 3
+// let user = prompt("Saisie le prix de ton article");
+// let solde = prompt("Saisie le pourcentage de la remise de cet article");
+// let resultatFirst = ( user * solde / 100 )
+// let resultatLast = (user - resultatFirst) 
+// 
+// if (user <= 0){
+//    console.log("Veuillez saisir une valeur valide de votre article !")
+// }
+// else {
+//    console.log("Après une réduction de" + " " + solde + " " +"%, le prix final est de" + " " + resultatLast + " euros")
+// }
+//
+// --------------- EXERCICE VIENNOISERIES --------------
+// FOR
+//for (let i = 1; i <= 10; i++) {
+//    console.log(`Viennoiserie numéro ${i}`);
+//}
+// WILE
+//let viennoiserie = 10;
+//
+//while (viennoiserie > 0) { 
+//    console.log("Il me reste " + viennoiserie + " " + "viennoiseries");
+//    viennoiserie--; 
+//}
+//console.log("Je n'en ai plus ! Faut que j'en rachete");
+//
+// FOREACH
+//let viennoieries = ["pain au raison", "pain au chocolat", "croissant"];
+//
+//viennoieries.forEach(function(nomviennoiserie){
+//    console.log("Aujourd'hui, on a :" + " " + nomviennoiserie);
+//});
+// ------------------------------------------ //
--- script.js
+++ script.js
@@ -1,42 +1,87 @@
-// Exerice 3
-// let user = prompt("Saisie le prix de ton article");
-// let solde = prompt("Saisie le pourcentage de la remise de cet article");
-// let resultatFirst = ( user * solde / 100 )
-// let resultatLast = (user - resultatFirst) 
-// 
-// if (user <= 0){
-//    console.log("Veuillez saisir une valeur valide de votre article !")
-// }
-// else {
-//    console.log("Après une réduction de" + " " + solde + " " +"%, le prix final est de" + " " + resultatLast + " euros")
-// }
-//
-// ------------ FOR -----------
-//for ( let PointsVie = 1; PointsVie <= 5 ; PointsVie++){
-//
-//   console.log("Mes points de vie après la potion de santé:" + PointsVie);
-//}
-// ----------- WHILE ----------
-//let pointvies = 10;
-//
-//while ( pointvies > 0 ){
-//
-//    console.log("Il me reste" + " " + pointvies + " " + "points de vie. Continue à jouer Harry !")
-//    pointvies--;
-//}
-//console.log("Game Over !")
-//
-//
-// --------------- BREAK --------------
-//let compteur = 1;
-//
-//while(true){
-//    console.log("Degat au joueur" + " " + compteur +"pts de perdu")
-//
-//    if(compteur === 5){
-//
-//      console.log("L'attaque est terminée");
-//        break
-//    }
-//    compteur ++;
-//}
+// ----------------- GAMES --------------------- //
+//     BIENVENUE SUR LE JEUX D'HARRY POTTER
+// --------------------------------------------- //
+// LANCEMENT DU JEUX AFFICHAGE
+let launchGames = prompt("Veux-tu faire une partie du jeux Harry Potter ? Ecrit 'y' pour OUI ou 'n' pour NON" );
+
+    if (launchGames === "y") {
+    // DEBUT DU JEUX "BIENVENUE - WELCOME"
+    alert(" Bienvenue dans le jeux d'Harry Potter !" );
+    // INFORMATION POUR OUVRIR LA CONSOLE
+    alert("Ouvre ta console de ton navigateur pour voir ton aventure (Raccourci F12)" );
+
+    // ENTRAINEMENT INFORMATIONS
+    console.log(" Debut de l'entrainement de ton héro !" );
+    console.log(" Ton héro doit effectuer une série de 5 entrainements avant de partir au combat !" );
+
+    // DEMANDE A L'UTILISATEUR LE NOMBRE DE SEANCE POUR OBTENIR DE L'ENERGIE
+    let nbSeances = prompt("Combien de séances d'entrainement veux-tu réaliser ?")
+
+    // L'UTILISATEUR NE POSSEDE AUCUNE ENERGIE
+    let energie = 0;
+
+    // ENTRAINEMENT -> EN FONCTION DU CHOIX DE L'UTILISATEUR
+    for ( energie; energie <= nbSeances; (energie++) ) {
+        console.log( "Entrainement " + energie + " terminé ! Bravo !" );
+    }
+    // RETIRER 1 POINT POUR COMMENCER A ZERO
+    const heroEnergie = (energie - 1)
+
+    // INFORMATION DU RENDU DE L'ENTRAINEMENT
+    console.log( "Ton entrainement est terminé ! Tu peux désormais combattre contre des ennemis !" );
+    alert(" Tu as obtenu" + " " + heroEnergie + " " + "énergies" );
+    console.log( " Tu as obtenu" + " " + heroEnergie + " " + "énergies" );
+    // INFORMATION ENNEMIES EN APPROCHE
+    alert("Harry aperçoit 3 ennemies !")
+
+    // CHOIX DE L'UTILISATEUR
+    let choixUser = prompt("Que veux-tu faire ?  Combattre 'y' ou 'n'" );
+
+    if (choixUser == "y"){
+
+        // COMBAT - CONTRE 3 ENNEMIS
+        console.log( "Lancement du combat !" );
+        console.log( "Il y a 3 ennemies devant toi ! Attention !" );
+
+        // COMBAT - LE JOUEUR PERD TOUTE SON ENERGIE
+        const choixUser = prompt( "Combien d'ennemies veux-tu combattre ?" );
+        // 
+        while (heroEnergie >= choixUser) { 
+            // INFORMATION
+            alert("Le premier ennemie a été abattu ! Bravo !");
+            // REGARDER ENERGIE PDT COMBAT
+            let consultEnergie = prompt( "Veux-tu consulter ton énergie ? Ecrit 'y' pour OUI ou 'n' pour NON" );
+            
+            if (consultEnergie == "y") {
+                const heroEnergiecombat = (heroEnergie - 1)
+                alert("Il te reste " + heroEnergiecombat + "énergie");
+                console.log("Il te reste " + heroEnergiecombat + " " + "énergie");
+            } else {
+                alert("L'aventure continue !");
+            }
+            // INFORMATION FINAL DU COMBAT
+            alert("Tu as tué les " + choixUser + " ennemis, Bravo !");
+            break;
+        }
+
+
+
+
+        // EN COURS
+        // RECOMPENSE COFFRES
+        let ObjetCoffre = [ "Epée legendaire", "Potion magique", "Bouclier en or" ];
+        ObjetCoffre.forEach(function(objetcoffre){
+        console.log("Objet récupéré :" + " " + objetcoffre);
+        });
+
+        // INFORMATION ENERGIE APRES COMBAT
+        console.log( "Alerte ! Tu n'as plus d'energie !" );
+        console.log( "Tu dois refaire un entrainement pour continuer l'aventure !" );
+    } else{
+        alert("Dommage ! Tu aurais pu essayer..." );
+        alert("Relance le jeux si tu veux refaire une partie" );
+    }
+// ARRET DU JEUX
+} else{
+    alert("Reviens quand tu veux, si tu veux faire une partie de ce jeux !")
+}
